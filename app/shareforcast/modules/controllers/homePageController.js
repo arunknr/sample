cf0c01e82985e82f68bf38a807a2bfbf51bf6574
arathyjan <arathyjanardhanan@gmail.com>
2015-09-05 20:59:58 +0530

    Adding few messages around the page

--- app/shareforcast/modules/controllers/homePageController.js
+++ app/shareforcast/modules/controllers/homePageController.js
@@ -3,6 +3,6 @@
 angular.module('com.shareForcast.controllers', ['com.shareForcast.directives'])
     .controller('HomePageController', ['$scope',
         function ($scope) {
-            $scope.members = ['EPS', 'Face Value', 'Market Cap', 'Price', '>', '<', '=', '<=', '>=', 'AND', 'OR'];
+            $scope.members = ['EPS', 'Face Value', 'Market Cap', 'Maggi','Map', 'Price', '>', '<', '=', '<=', '>=', 'AND', 'OR'];
         }
     ]);
