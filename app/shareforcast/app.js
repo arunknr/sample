1350d6322a74dada46aca18e8ce93f96f54a8bc0
arathyjan <arathyjanardhanan@gmail.com>
2015-09-12 17:47:32 +0530

    fixing empty response issue at angularJS end

--- app/shareforcast/app.js
+++ app/shareforcast/app.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('shareForcast', ['com.shareForcast']);
-angular.module('shareForcast').config(['$routeProvider', '$httpProvider', function ($routeProvider) {
+angular.module('shareForcast').config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
         $routeProvider.when('/login', {templateUrl: 'modules/views/login.html', controller: 'LoginController'});
         $routeProvider.when('/home', {templateUrl: 'modules/views/home.html', controller: 'HomePageController'});
         $routeProvider.otherwise({redirectTo: '/login'});
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }]);
